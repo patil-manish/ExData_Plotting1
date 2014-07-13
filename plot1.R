c00cecc58f27e9780f053f950e66f77d585b564c
Manish Patil <patil.manish@gmail.com>
2014-07-14 04:35:56 +0530

    Commit for week 1 project

---
+++ plot1.R
@@ -0,0 +1,6 @@
+df <- read.table("household_power_consumption.txt",header=T, sep=";",na.strings="?",nrows=70000, as.is=c(1,2))
+filt <- subset(df,Date=="1/2/2007"|Date=="2/2/2007")
+par(mfrow = c(1,1))
+hist(filt$Global_active_power,col="red",main="Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, file="plot1.png")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,8 @@
+df <- read.table("household_power_consumption.txt",header=T, sep=";",na.strings="?",nrows=70000, as.is=c(1,2))
+filt <- subset(df,Date=="1/2/2007"|Date=="2/2/2007")
+filt$dtime <- strptime(paste(filt$Date,filt$Time,sep=" "), "%d/%m/%Y %H:%M:%S")
+par(mfrow = c(1,1))
+plot(filt$dtime, filt$Global_active_power, type="n", xlab="", ylab="Global Active Power (kilowatts)")
+lines(filt$dtime, filt$Global_active_power, type="l")
+dev.copy(png, file="plot2.png")
+dev.off()
