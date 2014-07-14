e3da608db507ab7a60047a6fd02717ae11a669e3
Manish Patil <patil.manish@gmail.com>
2014-07-14 13:24:32 +0530

    added comment about the match/mismatch wrt reference plots

--- plot3.R
+++ plot3.R
@@ -1,3 +1,5 @@
+# Plot matching reference in terms of Data, plot type and colors, Axis, Labels, Legend text and color
+# Label and Legend position is not exactly identical wrt reference plots
 df <- read.table("household_power_consumption.txt",header=T, sep=";",na.strings="?",nrows=70000, as.is=c(1,2))
 filt <- subset(df,Date=="1/2/2007"|Date=="2/2/2007")
 filt$dtime <- strptime(paste(filt$Date,filt$Time,sep=" "), "%d/%m/%Y %H:%M:%S")
